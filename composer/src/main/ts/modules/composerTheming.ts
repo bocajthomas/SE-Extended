03fca75345d292a3c1de0d8faf16094c5886ca34
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2024-06-12 21:56:49 +0100

    refactor: composer theming

--- composer/src/main/ts/modules/composerTheming.ts
+++ composer/src/main/ts/modules/composerTheming.ts
@@ -3,25 +3,29 @@ import { defineModule } from "../types";
 import { interceptComponent } from "../utils"
 // Composer theme test
 export default defineModule({
-  name: "Theme Test",
+  name: "Composertheming",
   init() {
-    const baseStyle = new style.Style({
-      backgroundColor: "#ffffff",
-      background: "#000000", 
-    });
-
     interceptComponent(
-      "context_chrome_header/src/ChromeHeaderRenderer",
-      "ChromeHeaderRenderer",
-      {
-        onRenderBaseHeader: (_component, _args, render) => {
-          render();
-          jsx.beginRender(jsx.makeNodePrototype("View"));
-          jsx.setAttributeStyle("style", baseStyle);
-          jsx.endRender();
-        },
+      proxyProperty(require('composer_core/src/JSX').jsx, "setAttribute", (target, thisArg, argumentsList) => {
+        const key = argumentsList[0];
+        if (key == "style") {
+          console.log("setAttribute", key);
+          dumpObject(argumentsList[1]);
+        }
+        return Reflect.apply(target, thisArg, argumentsList);
+      })
+      proxyProperty(require('composer_core/src/JSX').jsx, "setAttributeString", (target, thisArg, argumentsList) => {
+      const key = argumentsList[0];
+      if (key == "backgroundColor") {
+        argumentsList[1] = "#ffffffff";
       }
-    );
-  },
-  enabled: true,
-});
+      if (key == "background") {
+        argumentsList[1] = "#ffffffff";
+      }
+      if (key == "tint") {
+        argumentsList[1] = "#ffffffff";
+      }
+      return Reflect.apply(target, thisArg, argumentsList);
+    })
+  }
+})
