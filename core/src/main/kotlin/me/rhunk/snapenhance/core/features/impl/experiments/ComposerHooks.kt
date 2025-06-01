cbf69b239c7216864d54e6a164e9d52ad1e60997
rhunk <101876869+rhunk@users.noreply.github.com>
2025-06-01 14:59:58 +0200

    feat(composer): custom self destruct snap delay
    Signed-off-by: rhunk <101876869+rhunk@users.noreply.github.com>

--- core/src/main/kotlin/me/rhunk/snapenhance/core/features/impl/experiments/ComposerHooks.kt
+++ core/src/main/kotlin/me/rhunk/snapenhance/core/features/impl/experiments/ComposerHooks.kt
@@ -120,7 +120,8 @@ class ComposerHooks: Feature("ComposerHooks") {
                 "operaDownloadButton" to context.config.downloader.operaDownloadButton.get(),
                 "bypassCameraRollLimit" to config.bypassCameraRollLimit.get(),
                 "showFirstCreatedUsername" to config.showFirstCreatedUsername.get(),
-                "composerLogs" to config.composerLogs.get()
+                "composerLogs" to config.composerLogs.get(),
+                "customSelfDestructSnapDelay" to config.customSelfDestructSnapDelay.get(),
             ))
         }
 
