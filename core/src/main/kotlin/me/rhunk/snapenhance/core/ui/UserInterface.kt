86234a62ba662ed18678a586145060f48a381edb
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2025-07-13 02:20:13 +0100

    fix: fonts

--- core/src/main/kotlin/me/rhunk/snapenhance/core/ui/UserInterface.kt
+++ core/src/main/kotlin/me/rhunk/snapenhance/core/ui/UserInterface.kt
@@ -5,6 +5,7 @@ import android.graphics.Typeface
 import android.util.TypedValue
 import android.view.Gravity
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import me.rhunk.snapenhance.core.ModContext
 import me.rhunk.snapenhance.core.util.hook.HookStage
 import me.rhunk.snapenhance.core.util.hook.hook
@@ -13,14 +14,13 @@ import me.rhunk.snapenhance.core.util.ktx.isDarkTheme
 class UserInterface(
     private val context: ModContext
 ) {
-    private val fontMap = mutableMapOf<Int, Int>()
+    private val fontMap = mutableMapOf<Int, Typeface>()
 
     val colorPrimary get() = if (context.androidContext.isDarkTheme()) 0xfff5f5f5.toInt() else 0xff212121.toInt()
     val actionSheetBackground get() = if (context.androidContext.isDarkTheme()) 0xff1e1e1e.toInt() else 0xffffffff.toInt()
 
-    val avenirNextTypeface: Typeface by lazy {
-        fontMap[600]?.let { context.resources.getFont(it) } ?: Typeface.MONOSPACE
-    }
+    val avenirNextFontId = 500
+    val avenirNextTypeface get() = fontMap[avenirNextFontId] ?: fontMap.entries.sortedBy { it.key }.firstOrNull()?.value ?: Typeface.DEFAULT
 
     fun dpToPx(dp: Int): Int {
         return (dp * context.resources.displayMetrics.density).toInt()
@@ -29,10 +29,6 @@ class UserInterface(
     @Suppress("unused")
     fun pxToDp(px: Int): Int {
         return (px / context.resources.displayMetrics.density).toInt()
-    }
-
-    fun getFontResource(weight: Int): Int? {
-        return fontMap[weight]
     }
 
     fun applyActionButtonTheme(view: TextView) {
@@ -64,7 +60,7 @@ class UserInterface(
         unhook = Resources::class.java.hook("getValue", HookStage.AFTER) { param ->
             val typedValue = param.argNullable<TypedValue>(1)?.takeIf {
                 it.resourceId != 0 &&
-                it.type == TypedValue.TYPE_STRING && it.string?.endsWith(".ttf") == true
+                        it.type == TypedValue.TYPE_STRING && it.string?.endsWith(".ttf") == true
             } ?: return@hook
 
             var offset = typedValue.resourceId.shr(8).shl(8)
