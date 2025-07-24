1c24348d4d146d505b3ceecb9b557b2e3231382b
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2025-07-24 16:36:51 +0100

    fix(disclaimer): button font size
    - fixed font size

--- app/src/main/kotlin/me/rhunk/snapenhance/ui/setup/screens/impl/DisclaimerScreen.kt
+++ app/src/main/kotlin/me/rhunk/snapenhance/ui/setup/screens/impl/DisclaimerScreen.kt
@@ -28,7 +28,7 @@ class DisclaimerScreen : SetupScreen() {
         DialogText("Welcome to the SE Extended Module."+"\nDisclaimer:"+"\n•Users must take full Responsibility for any risks associated with using this module."+"\n•The SE Extended Module can modify the behaviour of the Snapchat app. Users should carefully review the module's operations and make their own decisions regarding its usage."+"\n•The author nor the forker accepts no responsibility for any warnings or bans that may occur on your snapchat account that may arise from the use of the SE Extended module, and all consequences are to be borne by the users themselves.")
 
         Column (
-            modifier = Modifier.padding(16.dp),
+            modifier = Modifier.padding(15.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Button(
@@ -36,7 +36,7 @@ class DisclaimerScreen : SetupScreen() {
                     goNext()
                 }
             ) {
-                Text("I Agree", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                Text("I Agree", fontSize = 15.sp, fontWeight = FontWeight.Bold)
             }
         }
     }
